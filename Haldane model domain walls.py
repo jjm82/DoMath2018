9cee2f11933622c8420ae8d42656532cee696cfd
jjm82 <36308022+jjm82@users.noreply.github.com>
2018-07-02 15:23:52 -0400

    Add files via upload

--- Haldane model domain walls.py
+++ Haldane model domain walls.py
@@ -4,27 +4,35 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 from scipy.linalg import expm
-import IPython.display as IPdisplay
-import glob
-from PIL import Image as PIL_Image
-from images2gif import writeGif
+import cv2
+import os
 
-gif_filename = 'Haldane-model-animation'
-path = '/Users/jonathanmichala/All Documents/DOMath 2018/' + gif_filename
+video_name = 'Haldane-model-animation'
+path = '/Users/jonathanmichala/All Documents/DOMath 2018/' + video_name
 
 
 '''Parameters'''
-showmatrices = False
+topology = 1
+defect = True
+showmatrix = False
 animate = True
-end = 20
-frames = 5
+end = 40
+frames = 20
+title = 'defect4'
 
 localize = True
-mu=float(16)
-sig=float(1)
+mu=float(14)
+sig=float(10)
+kindex = 18
+
+printek = False
 
 n = 30
-m = 40 #both in number of cells
+m = 20 #both in number of cells
+top = m
+bottom = m-1
+edgest = bottom
+
 size = 2*n*m
 va0 = 1
 vb0 = -va0
@@ -38,10 +46,6 @@ hn = np.zeros((2*m,2*m), complex)
 h = np.zeros((size,size), complex)
 center = range(m/2 - m/4, m/2 + m/4)
 center = range(center[0] * 2, center[-1]*2)
-
-top = m
-bottom = m-1
-edgest = bottom
 
 kvals = np.linspace(0, 2 * np.pi, 50, True)
 k = []
@@ -57,32 +61,36 @@ for k1 in kvals:
         j = i
 
         if i%2==0:
-            if i in center:
+            if i in center and topology != 0:
                 hn[i,j]= va1 + t21 * (np.exp(1j*(p+k1))+np.exp(1j*(-p-k1)))
                 hn[i,(j-2) % (2*m)]= t21 * (np.exp(1j*(p))+np.exp(1j*(-p+k1)))
                 hn[i,(j+2) % (2*m)]= t21 * (np.exp(1j*(-p))+np.exp(1j*(p-k1)))
                 hn[(i+2) % (2*m), j] = t21 * (np.exp(1j*(-p))+np.exp(1j*(p-k1)))
                 hn[(i-2) % (2*m), j] = t21 * (np.exp(1j*(p))+np.exp(1j*(-p+k1)))
             else:
-                hn[i,j]= va0
+                if topology == 0:
+                    hn[i,j]= 3 * va0
+                else: hn[i,j]= va0 #because t20 = 0
             hn[i,(j-1) % (2*m)]= t
             hn[i,(j+1) % (2*m)]= t * (1+np.exp(1j*(-k1)))
             
         if i%2==1:
-            if i in center:
+            if i in center and topology != 0:
                 hn[i,j]= vb1 + t21 * (np.exp(1j*(p-k1))+np.exp(1j*(-p+k1)))
                 hn[i,(j-2) % (2*m)]= t21 * (np.exp(1j*(-p))+np.exp(1j*(p+k1)))
                 hn[i,(j+2) % (2*m)]= t21 * (np.exp(1j*(p))+np.exp(1j*(-p-k1)))
                 hn[(i+2) % (2*m), j]= t21 * (np.exp(1j*(p))+np.exp(1j*(-p-k1)))
                 hn[(i-2) % (2*m), j]= t21 * (np.exp(1j*(-p))+np.exp(1j*(p+k1)))
             else:
-                hn[i,j]= vb0
+                if topology == 0:
+                    hn[i,j]= 3 * vb0
+                else: hn[i,j]= vb0 #because t20 = 0
             hn[i,(j-1) % (2*m)]= t * (1+np.exp(1j*(k1)))
             hn[i,(j+1) % (2*m)]= t
              
 
     evalues, evectors = np.linalg.eigh(hn)
-    if count == 17:
+    if count == kindex:
         evalue = evalues[edgest].real
         state0 = evectors[:, edgest]
         statek = [k1]
@@ -96,22 +104,30 @@ for k1 in kvals:
 va = va1
 vb = vb1
 t2 = t21
+if topology == 0:
+    va = -va0
+    vb = -vb0
+    t2 = 0
 for i in range(size):
     if i%2==0:
-        h[i,i]=va
+        if defect and i/(2*m) in [3*n/4, 3*n/4 + 1] and i%(2*m) == center[0]:
+            h[i,i] = 1000
+        else: h[i,i]=va
         if i%(2*m)!=0 and i%(2*m)!=(2*m)-1 and i%(2*m)!=1 and i%(2*m)!=(2*m)-2:
             h[i,(i-2*m+1)%size]=t
             h[i,(i-1)%size]=t
             h[i,(i+1)%size]=t
-            h[i,(i+2*m)%size]=t2*np.exp(1j*p) #i just put in t2 times p or minus p for now to not mess with exponentials yet
+            h[i,(i+2*m)%size]=t2*np.exp(1j*p)
             h[i,(i-2*m+2)%size]=t2*np.exp(1j*p)
             h[i,(i-2)%size]=t2*np.exp(1j*p)
             h[i,(i-2*m)%size]=t2*np.exp(-1j*p)
             h[i,(i+2*m-2)%size]=t2*np.exp(-1j*p)
             h[i,(i+2)%size]=t2*np.exp(-1j*p)
 
-    else: 
-        h[i,i]=vb
+    else:
+        if defect and i/(2*m) in [3*n/4, 3*n/4 + 1] and i%(2*m) == center[0]:
+            h[i,i] = 1000
+        else: h[i,i]=vb
         if i%(2*m)!=0 and i%(2*m)!=2*m-1 and i%(2*m)!=1 and i%(2*m)!=2*m-2:
             h[i,(i+2*m-1)%size]=t
             h[i,(i-1)%size]=t
@@ -184,10 +200,9 @@ for i in range(size):
                 h[i-2*m+2,i] = h[(i+2*m-2)%size,i] = h[(i-2*m)%size,i] = t2*np.exp(1j*p)
                 h[i-2,i] = h[(i-4*m+2)%size,i] = h[(i+2*m)%size,i] = t2*np.exp(-1j*p)
 
-if showmatrices:
+
+if showmatrix:
     plt.matshow(h.real)
-    for time in np.linspace(.1,end,frames):
-        plt.matshow(expm(-1j*time*h).real)
 
 state0firstrow = state0
 for i in range(1,n):
@@ -207,9 +222,9 @@ Y = range(n)
 X, Y = np.meshgrid(X,Y)
 fig = plt.figure()
 ax = fig.add_subplot(111, projection = '3d')
-#ax.set_zlim(-.05,.05)
+ax.set_zlim(-.03,.03)
 ax.azim = -.1
-ax.elev = .1
+ax.elev = 90.1
 ax.dist = 8
 ax.plot_surface(X, Y, E, cmap=cm.RdBu, linewidth=0, antialiased=False)
 plt.savefig(path + '/img00.png')
@@ -223,23 +238,32 @@ if animate:
         E = E.reshape(n,2*m)
         fig2 = plt.figure()
         ax = fig2.add_subplot(111, projection = '3d')
-        #ax.set_zlim(-.05,.05)
+        ax.set_zlim(-.03,.03)
         ax.azim = -.1
-        ax.elev = .1
+        ax.elev = 90.1
         ax.dist = 8
         ax.plot_surface(X, Y, E, cmap=cm.RdBu, linewidth=0, antialiased=False)
-        plt.savefig(path + '/img' + str(num).zfill(2) + '.png')
+        plt.savefig(path + '/img' + str(num).zfill(2) + '.jpg', format='jpg')
+        plt.close(fig2)
 
         num += 1
-    
-    '''plt.close()
-    images = [PIL_Image.open(image) for image in glob.glob(path + '/*.png')]
-    file_path_name = path + '.gif'
-    writeGif(file_path_name, images, duration=0.3)
-    IPdisplay.Image(url=file_path_name)'''
+
+    images = [cv2.imread(os.path.join(path, img)) for img in os.listdir(path) if img.endswith(".jpg")]
+    height,width,layers = images[0].shape
+
+    video = cv2.VideoWriter(path + '/' + title + '.mp4',-1,15,(width,height))
+
+    for j in range(len(images)):
+        video.write(images[j])
+
+    cv2.destroyAllWindows()
+    video.release()
 
 fig = plt.figure()
 plt.scatter(k,e)
 plt.scatter(statek, stateeval, color='red')
-
+if printek:
+    print 'E: ', stateeval[0]
+    print 'k: ', statek[0]
+    print 'E/k: ', stateeval[0] / statek[0]
 plt.show()
